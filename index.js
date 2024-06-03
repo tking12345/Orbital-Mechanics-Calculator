94f34359b9e3a3fbc93daeddac9facb50585d71b
Tyler King <tjking.1125@gmail.com>
2024-06-03 11:48:29 -0400

    Facelift

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const GMk = 3.5316e12;
 const MeanRe = 6.371e6;
 const MeanRk = 600000;
 
+
 MU = 0;
 MR = 0;
 
@@ -18,7 +19,7 @@ function WhatPlanet(){
     
      else { 
         Planet = 0;  
-        document.getElementById("error").innerHTML = "Must choose the body you are orbiting!";   
+        document.getElementById("error").innerHTML = "<h4 id='error' style= 'color:red'>Must choose the body you are orbiting!</h4>";   
      }   
 
     if(Planet == "Earth"){
@@ -38,6 +39,7 @@ function calculate(){
 
     //vis viva calculation 
     if (Planet != 0){
+        document.getElementById("error").innerHTML = null;
 
         var Apoapsis1 = Number(document.getElementById("Apo").value);
         var Periapsis1 = Number(document.getElementById("Per").value);
@@ -64,87 +66,94 @@ function calculate(){
 }
 
 function calculateHohmann(){
-
     WhatPlanet();
-    var Periapsis1 = Number(document.getElementById("Per").value);
-    var Apoapsis1 = Number(document.getElementById("Apo").value);
-    var Apoapsis2 = Number(document.getElementById("Apo2").value);
-    var Periapsis2 = Number(document.getElementById("Per2").value);
-    var a1 = (((Apoapsis1 + Periapsis1) + (2 * MR))/2);
+    
+    if (Planet != 0){
+    
+        document.getElementById("error").innerHTML = null;
+        var Periapsis1 = Number(document.getElementById("Per").value);
+        var Apoapsis1 = Number(document.getElementById("Apo").value);
+        var Apoapsis2 = Number(document.getElementById("Apo2").value);
+        var Periapsis2 = Number(document.getElementById("Per2").value);
+        var a1 = (((Apoapsis1 + Periapsis1) + (2 * MR))/2);
 
-    //Burn 1
-    var a2 = (((Apoapsis2 + Periapsis1) + (2 * MR))/2);
-    var VPE1 = Math.sqrt((MU * ((2 / (Periapsis1 + MR))-(1 / a1)))); 
-    var VAP2 = Math.sqrt((MU * ((2 / (Apoapsis2 + MR))-(1 / a2)))); 
-    var VPE1_1 = Math.sqrt(MU * ((2 / (Periapsis1 + MR))-(1 / a2)));
-    var deltaV = Math.abs(VPE1_1 - VPE1);
-    var Period2 = ((2*Math.PI)*Math.sqrt((Math.pow(a2,3)/MU)))/60;
+        //Burn 1
+        var a2 = (((Apoapsis2 + Periapsis1) + (2 * MR))/2);
+        var VPE1 = Math.sqrt((MU * ((2 / (Periapsis1 + MR))-(1 / a1)))); 
+        var VAP2 = Math.sqrt((MU * ((2 / (Apoapsis2 + MR))-(1 / a2)))); 
+        var VPE1_1 = Math.sqrt(MU * ((2 / (Periapsis1 + MR))-(1 / a2)));
+        var deltaV = Math.abs(VPE1_1 - VPE1);
+        var Period2 = ((2*Math.PI)*Math.sqrt((Math.pow(a2,3)/MU)))/60;
 
-    newApoapsis = Math.max(Apoapsis2,Periapsis1);
-    newPeriapsis = Math.min(Apoapsis2, Periapsis1);
-    newVAP = Math.min(VAP2, VPE1_1);
-    newVEP = Math.max(VAP2, VPE1_1);
+        newApoapsis = Math.max(Apoapsis2,Periapsis1);
+        newPeriapsis = Math.min(Apoapsis2, Periapsis1);
+        newVAP = Math.min(VAP2, VPE1_1);
+        newVEP = Math.max(VAP2, VPE1_1);
 
-    results2 = "Transfer orbit after burn 1: </br>" + 
-    "Apoapsis: " + newApoapsis.toLocaleString() + " m</br>" + 
-    "Periapsis: " + newPeriapsis.toLocaleString() + " m</br>" +
-    "Semi Major Axis: " + a2.toLocaleString() + " m</br>" + 
-    "V@AP: " + newVAP.toLocaleString() + " m/s</br>" +
-    "V@PE: " + newVEP.toLocaleString() +" m/s</br>"+
-    "&Delta;V: " + deltaV.toLocaleString() + " m/s</br>" + 
-    "Orbital Period: " + Period2.toFixed(2) + " Minutes</br>"+
-    "Time in transfer orbit: " + (Period2/2).toFixed(2) + " Minutes</br> ";
+        results2 = "Transfer orbit after burn 1: </br>" + 
+        "Apoapsis: " + newApoapsis.toLocaleString() + " m</br>" + 
+        "Periapsis: " + newPeriapsis.toLocaleString() + " m</br>" +
+        "Semi Major Axis: " + a2.toLocaleString() + " m</br>" + 
+        "V@AP: " + newVAP.toLocaleString() + " m/s</br>" +
+        "V@PE: " + newVEP.toLocaleString() +" m/s</br>"+
+        "&Delta;V: " + deltaV.toLocaleString() + " m/s</br>" + 
+        "Orbital Period: " + Period2.toFixed(2) + " Minutes</br>"+
+        "Time in transfer orbit: " + (Period2/2).toFixed(2) + " Minutes</br> ";
 
-    document.getElementById("hohmann-transfer-output").innerHTML = results2
+        document.getElementById("hohmann-transfer-output").innerHTML = results2
 
-    newApoapsis = null;
-    newPeriapsis = null;
-    newVAP = null;
-    newVEP = null;
-    //Burn 2
-    var a3 = (((Apoapsis2 + Periapsis2) + (2 * MR)) / 2);
+        newApoapsis = null;
+        newPeriapsis = null;
+        newVAP = null;
+        newVEP = null;
+        //Burn 2
+        var a3 = (((Apoapsis2 + Periapsis2) + (2 * MR)) / 2);
 
 
-    var VAP2_2 =Math.sqrt((MU * ((2 / (Apoapsis2 + MR))-(1 / a3)))); 
-    var VPE2 = Math.sqrt(MU * ((2 / (Periapsis2 + MR))-(1 / a3))); 
-    var deltaV2 = Math.abs(VAP2_2 -VAP2) ;
-    var Period3 = (((2*Math.PI)*Math.sqrt((Math.pow(a3,3)/MU)))/60);
-     
-    newApoapsis = Math.max(Periapsis2, Apoapsis2);
-    newPeriapsis = Math.min(Periapsis2, Apoapsis2);
-    newVAP = Math.min(VAP2_2, VPE2);
-    newVEP = Math.max(VAP2_2, VPE2);
+        var VAP2_2 =Math.sqrt((MU * ((2 / (Apoapsis2 + MR))-(1 / a3)))); 
+        var VPE2 = Math.sqrt(MU * ((2 / (Periapsis2 + MR))-(1 / a3))); 
+        var deltaV2 = Math.abs(VAP2_2 -VAP2) ;
+        var Period3 = (((2*Math.PI)*Math.sqrt((Math.pow(a3,3)/MU)))/60);
+        
+        newApoapsis = Math.max(Periapsis2, Apoapsis2);
+        newPeriapsis = Math.min(Periapsis2, Apoapsis2);
+        newVAP = Math.min(VAP2_2, VPE2);
+        newVEP = Math.max(VAP2_2, VPE2);
 
-    results3 = "New orbit after burn 2: </br>" + 
-    "Apoapsis: " + newApoapsis.toLocaleString()+ " m</br>" + 
-    "Periapsis: " + newPeriapsis.toLocaleString() + " m</br>" +
-    "Semi Major Axis: " + a3.toLocaleString() + " m</br>" + 
-    "V@AP: " + VAP2_2.toLocaleString() + " m/s</br>" +
-    "V@PE: " + VPE2.toLocaleString() +" m/s</br>"+
-    "&Delta;V: " + deltaV2.toLocaleString() + " m/s</br>" +
-    "Total &Delta;V: " + (deltaV+deltaV2) + " m/s</br>" +
-    "Orbital Period: " + Period3.toFixed(3) + " Minutes</br>";
+        results3 = "New orbit after burn 2: </br>" + 
+        "Apoapsis: " + newApoapsis.toLocaleString()+ " m</br>" + 
+        "Periapsis: " + newPeriapsis.toLocaleString() + " m</br>" +
+        "Semi Major Axis: " + a3.toLocaleString() + " m</br>" + 
+        "V@AP: " + VAP2_2.toLocaleString() + " m/s</br>" +
+        "V@PE: " + VPE2.toLocaleString() +" m/s</br>"+
+        "&Delta;V: " + deltaV2.toLocaleString() + " m/s</br>" +
+        "Total &Delta;V: " + (deltaV+deltaV2) + " m/s</br>" +
+        "Orbital Period: " + Period3.toFixed(3) + " Minutes</br>";
 
-    document.getElementById("new-orbit-output").innerHTML = results3
+        document.getElementById("new-orbit-output").innerHTML = results3
+    }
 }
 
 function calculateCoplaner(){
 
     WhatPlanet();
-    Periapsis1 = Number(document.getElementById("Per").value);
-    Apoapsis1 = Number(document.getElementById("Apo").value);
-    Apoapsis2 = Number(document.getElementById("Apo2").value);
-    Periapsis2 = Number(document.getElementById("Per2").value);
-    a1 = (((Apoapsis1 + Periapsis1) + (2 * MR))/2);
-    a3 = (((Apoapsis2 + Periapsis2) + (2 * MR)) / 2);
-    w = Math.sqrt((MU/Math.pow(a3, 3)));
-    at = (a1+a3)/2;
-    TOF = Math.PI * Math.sqrt(Math.pow(at, 3)/MU);
-    lead = (180 - ((180/Math.PI)*(w * TOF)));
+    if (Planet != 0){
+        document.getElementById("error").innerHTML = null;
+        Periapsis1 = Number(document.getElementById("Per").value);
+        Apoapsis1 = Number(document.getElementById("Apo").value);
+        Apoapsis2 = Number(document.getElementById("Apo2").value);
+        Periapsis2 = Number(document.getElementById("Per2").value);
+        a1 = (((Apoapsis1 + Periapsis1) + (2 * MR))/2);
+        a3 = (((Apoapsis2 + Periapsis2) + (2 * MR)) / 2);
+        w = Math.sqrt((MU/Math.pow(a3, 3)));
+        at = (a1+a3)/2;
+        TOF = Math.PI * Math.sqrt(Math.pow(at, 3)/MU);
+        lead = (180 - ((180/Math.PI)*(w * TOF)));
 
-    results = "Phase angle: " + lead.toFixed(3) + "&deg;</br>";
+        results = "Phase angle: " + lead.toFixed(3) + "&deg;</br>";
 
-    document.getElementById("coplaner-output").innerHTML = results;
+        document.getElementById("coplaner-output").innerHTML = results;
+    }
 }
     
     
